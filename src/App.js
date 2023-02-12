e7ab36f18cce178f92b37a4e1176571bad09f082
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2023-02-12 15:18:49 +0100

    add function clear page

--- src/App.js
+++ src/App.js
@@ -3,38 +3,43 @@ import { useForm } from "react-hook-form";
 import "./App.css"
 import { MantineProvider, Button, Box } from '@mantine/core';
 
-function App() {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = async (data) => {
-    const formData = new FormData();
-    formData.append("file", data.file[0]);
-
-    const res = await fetch("http://localhost:5055/upload-file", {
-      method: "POST",
-      body: formData,
-    }).then((res) => res.json());
-    alert(JSON.stringify(`${res.message}, status: ${res.status}`));
-  };
-
-  return (
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Box className="App">
-        <Box className="Header">CSV UPLOAD ONLY</Box>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Box className="file-input">
-                <Button>
-                    <input className="custom-file-input" type="file" accept={".csv"}  {...register("file")} />
-                </Button>
-
-            </Box>
-            <Button mt={20} type="submit">Upload</Button>
-        </form>
-      </Box>
-      </MantineProvider>
-
-  );
+function refreshPage() {
+    setTimeout(() => {
+        window.location.reload();
+    }, 1000);
 }
 
-export default App;
+export default function App() {
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = async (data) => {
+            const formData = new FormData();
+            formData.append("file", data.file[0]);
+
+            const res = await fetch("http://localhost:5055/upload-file", {
+                method: "POST",
+                body: formData,
+            }).then((res) => res.json());
+            alert(JSON.stringify(`${res.message}, status: ${res.status}`));
+        };
+
+        return (
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <Box className="App">
+                    <Box className="Header">CSV UPLOAD ONLY</Box>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Box className="file-input">
+                            <Button>
+                                <input className="custom-file-input" type="file"
+                                       accept={".csv"}  {...register("file")}  />
+                            </Button>
+                        </Box>
+                        <Button onClick={refreshPage} mt={20} type="submit">Upload</Button>
+                    </form>
+                </Box>
+            </MantineProvider>
+
+        );
+    }
+
 
